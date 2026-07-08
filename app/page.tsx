b9acfc39a1e7d99979dcb29de0c59041bcca768d
khusanov ibrokhimjon <ibrohimjonhusanov1992@gmail.com>
2026-07-08 16:50:59 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import ProductGrid from "../components/ProductGrid";
+import Footer from "../components/Footer";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Header />
       <Hero />
       <ProductGrid />
+      <Footer />
     </>
   );
 }
