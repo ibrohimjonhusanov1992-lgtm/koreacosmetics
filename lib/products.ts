c7ae4a5d157abcf43e265fea0777416c5fb13e7b
khusanov ibrokhimjon <ibrohimjonhusanov1992@gmail.com>
2026-07-08 16:46:06 +0900

    Create products.ts

---
+++ lib/products.ts
@@ -0,0 +1,26 @@
+export const demoProducts = [
+  {
+    id: "1",
+    name: "Beauty of Joseon Relief Sun",
+    price: 199000,
+    image: "",
+  },
+  {
+    id: "2",
+    name: "COSRX Snail 96 Essence",
+    price: 289000,
+    image: "",
+  },
+  {
+    id: "3",
+    name: "SKIN1004 Centella Ampoule",
+    price: 269000,
+    image: "",
+  },
+  {
+    id: "4",
+    name: "Anua Heartleaf Toner",
+    price: 249000,
+    image: "",
+  },
+];
