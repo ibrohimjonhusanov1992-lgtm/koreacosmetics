440273b917d985e0f69808e0a10f2f6419225731
khusanov ibrokhimjon <ibrohimjonhusanov1992@gmail.com>
2026-07-08 16:04:17 +0900

    Create seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+const products = [
+  {
+    name: "Beauty of Joseon Relief Sun",
+    price: 199000,
+  },
+  {
+    name: "COSRX Snail 96 Essence",
+    price: 289000,
+  },
+  {
+    name: "SKIN1004 Centella Ampoule",
+    price: 269000,
+  },
+  {
+    name: "Anua Heartleaf Toner",
+    price: 249000,
+  },
+];
+
+console.log(products);
